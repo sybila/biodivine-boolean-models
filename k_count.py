1037bf8e8972ad9b477dbf5754692046e6a5b1a1
Samuel Pastva <sam.pastva@gmail.com>
2024-09-24 10:50:44 +0200

    Add a script to count the number of regulations, variables and non-constant variables.

---
+++ k_count.py
@@ -0,0 +1,22 @@
+import os
+from biodivine_aeon import *
+
+# This script counts the total number of variables,
+# regulators, and variables that are not inputs/constants.
+
+total_regulations = 0
+total_variables = 0
+total_non_inputs = 0
+for file in os.listdir('./sbml-true'):
+	if not file.endswith('.sbml'):
+		continue
+	bn = BooleanNetwork.from_file('./sbml-true/'+file)
+	for var in bn.variables():
+		total_variables += 1
+		regs = bn.regulators(var)
+		total_regulations += len(regs)
+		if len(regs) > 0:
+			total_non_inputs += 1
+print(total_regulations)
+print(total_variables)
+print(total_non_inputs)
