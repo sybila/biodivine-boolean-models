79f81bc780ac57797860244e021ce26115c37c58
Natalia Florekova <xflorek1@fi.muni.cz>
2025-05-27 14:10:51 +0200

    Ginsim downloader added

--- bbm-py/biodivine_bbm/model_downloader.py
+++ bbm-py/biodivine_bbm/model_downloader.py
@@ -3,6 +3,7 @@ import json
 import csv
 import os
 import glob
+import subprocess
 
 from MissingId import MissingIdException
 
@@ -13,7 +14,7 @@ def check_source_file(bbm_id):
     matches = glob.glob(f'../../sources/{int(bbm_id):03}_*') #finds matching model directories
     files = os.listdir(matches[0])
     for file in files:
-        if file.startswith('source'):
+        if file.startswith('source') and 'cellcollective' in file:
             return None
     return matches[0]
 
@@ -30,6 +31,32 @@ def download_model(ccid, version, dest):
             print(f"Download failed: {e}")
 
 
+def check_ginsim_files(bbm_id):
+    matches = glob.glob(f'../../sources/{int(bbm_id):03}_*') #finds matching model directories
+    files = os.listdir(matches[0])
+    for file in files:
+        if file.startswith('source') and 'ginsim' in file:
+            return None
+    return matches[0]
+
+
+def download_ginsim_model(ginsim_id, version, dest):
+    ginsim_id = './GINsim.github.io/content/models/' + ginsim_id 
+    if ginsim_id.endswith('sbml'):
+        try:
+            subprocess.run(['cp', ginsim_id, dest])
+        except:
+            print(f'File {ginsim_id} could not be copied to {des}')
+    elif ginsim_id.endswith('zginml'):
+        ginsim_sbml_file = ginsim_id.replace('.zginml', '.sbml')
+        #sbml_file = ginsim_sbml_file.split('/')[-1]
+        try:
+            subprocess.run(['sudo', '../biolqm_docker/ginsim_to_sbml.sh', ginsim_id])
+            subprocess.run(['mv', ginsim_sbml_file, dest + '/source.ginsim.sbml'])
+        except:
+            print(f'File {ginsim_id} could not be converted into sbml file format')
+
+
 def download_new_models_in_mapping_table(mapping_table):
     for line in mapping_table:
         if line[0] == 'bbm_id':
@@ -37,14 +64,17 @@ def download_new_models_in_mapping_table(mapping_table):
         
         if line[0] == '':
             continue
-        if line[2] == '':
+        if line[2] == '' and line[4] == '':
             raise MissingIdException(line[0])
+
         if line[1] == 'cellcollective':
             new_folder = check_source_file(line[0])
             if new_folder:
                 download_model(line[2], line[3],  new_folder)
         elif line[1].lower() == 'ginsim':
-            return        
+            new_source = check_ginsim_files(line[0])
+            if new_source:
+                download_ginsim_model(line[4], line[5], new_source)
 
 
 def download_new_in_mapping_table():
